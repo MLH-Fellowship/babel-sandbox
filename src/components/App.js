a6c351be2c053742ec9541018719b7402af471bf
Ian Jennings <ian@meetjennings.com>
2020-07-29 19:56:22 -0500

    Paircast - https://app.paircast.io/c/WPLnOAkm5/4925859

--- src/components/App.js
+++ src/components/App.js
@@ -125,18 +125,7 @@ export const App = ({ defaultSource, defaultBabelConfig, defCustomPlugin }) => {
         </Menu.Item>
         <Dropdown item icon='wrench' simple>
           <Dropdown.Menu>
-            <Dropdown.Item>
-
-              <Icon name='dropdown' />
-              <span className='text'>New</span>
-
-              <Dropdown.Menu>
-                <Dropdown.Item>Document</Dropdown.Item>
-                <Dropdown.Item>Image</Dropdown.Item>
-              </Dropdown.Menu>
-
-            </Dropdown.Item>
-            <Dropdown.Item>Open</Dropdown.Item>
+            <Dropdown.Item>New Config</Dropdown.Item>
             <Dropdown.Item>Save...</Dropdown.Item>
             <Dropdown.Item>Edit Permissions</Dropdown.Item>
             <Dropdown.Divider />
