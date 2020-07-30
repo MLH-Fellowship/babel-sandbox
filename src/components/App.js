54be7668666114572219625a7b9185366ba520cb
Ian Jennings <ian@meetjennings.com>
2020-07-29 20:07:52 -0500

    Paircast - https://app.paircast.io/c/WPLnOAkm5/5615861

--- src/components/App.js
+++ src/components/App.js
@@ -140,7 +140,6 @@ export const App = ({ defaultSource, defaultBabelConfig, defCustomPlugin }) => {
 
             </Dropdown.Item>
             <Dropdown.Item>Save...</Dropdown.Item>
-            <Dropdown.Item>Edit Permissions</Dropdown.Item>
             <Dropdown.Divider />
             <Dropdown.Header>Export</Dropdown.Header>
             <Dropdown.Item>Share</Dropdown.Item>
