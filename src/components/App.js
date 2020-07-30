3d06bc048e11ac04754f8012a63ef6dc56d8fa2b
Ian Jennings <ian@meetjennings.com>
2020-07-29 20:42:12 -0500

    Paircast - https://app.paircast.io/c/WPLnOAkm5/7676003

--- src/components/App.js
+++ src/components/App.js
@@ -47,43 +47,44 @@ function CompiledOutput({
       <Grid.Column width={16}>
         <Segment>
           <Grid columns={2} relaxed='very'>
+          </Grid>
       </Grid.Column>
-          <Grid.Column width={8}>
-            <Menu attached='top' tabular inverted>
+        <Grid.Column width={8}>
+          <Menu attached='top' tabular inverted>
+            <Menu.Item>
+              plugin.js
+                </Menu.Item>
+            <Menu.Menu position='right'>
               <Menu.Item>
-                plugin.js
-                </Menu.Item>
-              <Menu.Menu position='right'>
-                <Menu.Item>
-                  <Icon name='close' size='' />
-                </Menu.Item>
-              </Menu.Menu>
-            </Menu>
-            <Wrapper>
-              <Config
-                value={
-                  config === Object(config)
-                    ? JSON.stringify(config, null, "\t")
-                    : config
-                }
-                onChange={onConfigChange}
-                docName="config.json"
-                config={{ mode: "application/json" }}
-              />
-            </Wrapper>
-          </Grid.Column>
-          <Grid.Column width={8}>
-            <Code
-              value={compiled?.code ?? ""}
-              docName="result.js"
-              config={{ readOnly: true, lineWrapping: true }}
-              isError={compiled?.error ?? false}
+                <Icon name='close' size='' />
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+          <Wrapper>
+            <Config
+              value={
+                config === Object(config)
+                  ? JSON.stringify(config, null, "\t")
+                  : config
+              }
+              onChange={onConfigChange}
+              docName="config.json"
+              config={{ mode: "application/json" }}
             />
-          </Grid.Column>
-          <FileSize>
-            {compiled?.size}b, {gzip}b
+          </Wrapper>
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <Code
+            value={compiled?.code ?? ""}
+            docName="result.js"
+            config={{ readOnly: true, lineWrapping: true }}
+            isError={compiled?.error ?? false}
+          />
+        </Grid.Column>
+        <FileSize>
+          {compiled?.size}b, {gzip}b
       </FileSize>
-          <Toggle onClick={removeConfig} />
+        <Toggle onClick={removeConfig} />
     </Grid.Row>
   );
 }
@@ -102,124 +103,124 @@ export const App = ({defaultSource, defaultBabelConfig, defCustomPlugin}) => {
   const debouncedSource = useDebounce(source, 125);
 
   const updateBabelConfig = useCallback((config, index) => {
-          setBabelConfig((configs) => {
-            const newConfigs = [...configs];
-            newConfigs[index] = config;
+        setBabelConfig((configs) => {
+          const newConfigs = [...configs];
+          newConfigs[index] = config;
 
-            return newConfigs;
-          });
+          return newConfigs;
+        });
   }, []);
 
   const removeBabelConfig = useCallback((index) => {
-          setBabelConfig((configs) => configs.filter((c, i) => index !== i));
+        setBabelConfig((configs) => configs.filter((c, i) => index !== i));
   }, []);
 
   let results = babelConfig.map((config, index) => {
     return (
       <CompiledOutput
-          source={debouncedSource}
-          customPlugin={enableCustomPlugin ? customPlugin : undefined}
-          config={config}
-          key={index}
-          onConfigChange={(config) => updateBabelConfig(config, index)}
-          removeConfig={() => removeBabelConfig(index)}
-        />
+        source={debouncedSource}
+        customPlugin={enableCustomPlugin ? customPlugin : undefined}
+        config={config}
+        key={index}
+        onConfigChange={(config) => updateBabelConfig(config, index)}
+        removeConfig={() => removeBabelConfig(index)}
+      />
     );
   });
 
   useEffect(() => {
-          let size = new Blob([debouncedSource], {type: "text/plain" }).size;
+        let size = new Blob([debouncedSource], {type: "text/plain" }).size;
     setSize(size);
     gzipSize(debouncedSource).then((s) => setGzip(s));
   }, [debouncedSource]);
 
   return (
-    <Root>
-          <Menu attached='top' inverted>
-            <Menu.Item>
-              <img src="https://d33wubrfki0l68.cloudfront.net/7a197cfe44548cc1a3f581152af70a3051e11671/78df8/img/babel.svg" />
-            </Menu.Item>
-            <Dropdown item icon='wrench' simple>
-              <Dropdown.Menu inverted>
-                <Dropdown.Item onClick={() => {
-                  setSource("const hello = 'world';");
-                }}>Load Example</Dropdown.Item>
-                <Dropdown.Item
-                  onClick={() =>
-                    setBabelConfig((configs) => [
-                      ...configs,
-                      configs[configs.length - 1],
-                    ])
-                  }>Add Config</Dropdown.Item>
-                <Dropdown.Item>
+      <Root>
+        <Menu attached='top' inverted>
+          <Menu.Item>
+            <img src="https://d33wubrfki0l68.cloudfront.net/7a197cfe44548cc1a3f581152af70a3051e11671/78df8/img/babel.svg" />
+          </Menu.Item>
+          <Dropdown item icon='wrench' simple>
+            <Dropdown.Menu inverted>
+              <Dropdown.Item onClick={() => {
+                setSource("const hello = 'world';");
+              }}>Load Example</Dropdown.Item>
+              <Dropdown.Item
+                onClick={() =>
+                  setBabelConfig((configs) => [
+                    ...configs,
+                    configs[configs.length - 1],
+                  ])
+                }>Add Config</Dropdown.Item>
+              <Dropdown.Item>
 
-                  <Icon name='dropdown' />
-                  <span className='text'>Add Plugin</span>
+                <Icon name='dropdown' />
+                <span className='text'>Add Plugin</span>
 
-                  <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => toggleCustomPlugin(!enableCustomPlugin)}>Custom</Dropdown.Item>
-                    <Dropdown.Item>Import</Dropdown.Item>
-                  </Dropdown.Menu>
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={() => toggleCustomPlugin(!enableCustomPlugin)}>Custom</Dropdown.Item>
+                  <Dropdown.Item>Import</Dropdown.Item>
+                </Dropdown.Menu>
 
-                </Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item>Save...</Dropdown.Item>
-                <Dropdown.Item>Share</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Menu>
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item>Save...</Dropdown.Item>
+              <Dropdown.Item>Share</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu>
 
-          <Grid celled='internally'>
+        <Grid celled='internally'>
+          <Grid.Row>
+
+            <Grid.Column width={16}>
+              <Menu attached='top' tabular inverted>
+                <Menu.Item>
+                  input.js
+              </Menu.Item>
+                <Menu.Menu position='right'>
+                  <Menu.Item>
+                    {size}b, {gzip}b
+                  </Menu.Item>
+                </Menu.Menu>
+              </Menu>
+              <Wrapper>
+                <Code
+                  value={source}
+                  onChange={(val) => setSource(val)}
+                  docName="source.js"
+                />
+              </Wrapper>
+            </Grid.Column>
+          </Grid.Row>
+          {enableCustomPlugin && (
             <Grid.Row>
-
               <Grid.Column width={16}>
+
                 <Menu attached='top' tabular inverted>
                   <Menu.Item>
-                    input.js
-              </Menu.Item>
+                    plugin.js
+                </Menu.Item>
                   <Menu.Menu position='right'>
-                    <Menu.Item>
-                      {size}b, {gzip}b
-                  </Menu.Item>
+                    <Menu.Item onClick={() => toggleCustomPlugin(false)}>
+                      <Icon name='close' size='' />
+                    </Menu.Item>
                   </Menu.Menu>
                 </Menu>
                 <Wrapper>
                   <Code
-                    value={source}
-                    onChange={(val) => setSource(val)}
-                    docName="source.js"
+                    value={customPlugin}
+                    onChange={(val) => setCustomPlugin(val)}
+                    docName="plugin.js"
                   />
                 </Wrapper>
               </Grid.Column>
             </Grid.Row>
-            {enableCustomPlugin && (
-              <Grid.Row>
-                <Grid.Column width={16}>
+          )}
+          {results}
+        </Grid>
 
-                  <Menu attached='top' tabular inverted>
-                    <Menu.Item>
-                      plugin.js
-                </Menu.Item>
-                    <Menu.Menu position='right'>
-                      <Menu.Item onClick={() => toggleCustomPlugin(false)}>
-                        <Icon name='close' size='' />
-                      </Menu.Item>
-                    </Menu.Menu>
-                  </Menu>
-                  <Wrapper>
-                    <Code
-                      value={customPlugin}
-                      onChange={(val) => setCustomPlugin(val)}
-                      docName="plugin.js"
-                    />
-                  </Wrapper>
-                </Grid.Column>
-              </Grid.Row>
-            )}
-            {results}
-          </Grid>
-
-        </Root >
+      </Root >
   );
 };
 
@@ -234,11 +235,11 @@ function useDebounce(value, delay) {
 
   React.useEffect(() => {
     const handler = setTimeout(() => {
-          setDebouncedValue(value);
+        setDebouncedValue(value);
     }, delay);
 
     return () => {
-          clearTimeout(handler);
+        clearTimeout(handler);
     };
   }, [delay, value]);
 
