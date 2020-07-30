b153beafcc771fb55bf2f6fb1880e0b976d6badc
Ian Jennings <ian@meetjennings.com>
2020-07-29 20:13:47 -0500

    Paircast - https://app.paircast.io/c/WPLnOAkm5/5970880

--- src/components/App.js
+++ src/components/App.js
@@ -155,80 +155,32 @@ export const App = ({ defaultSource, defaultBabelConfig, defCustomPlugin }) => {
 
       <Grid celled='internally'>
         <Grid.Row>
-          import React from 'react'
-import {Dropdown, Icon, Menu, Segment} from 'semantic-ui-react'
-
-// TODO: Update <Search> usage after its will be implemented
-
-          const MenuExampleAttached = () => (
-  <div>
-              <Menu attached='top'>
-                <Dropdown item icon='wrench' simple>
-                  <Dropdown.Menu>
-                    <Dropdown.Item>
-                      <Icon name='dropdown' />
-                      <span className='text'>New</span>
-
-                      <Dropdown.Menu>
-                        <Dropdown.Item>Document</Dropdown.Item>
-                        <Dropdown.Item>Image</Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>Open</Dropdown.Item>
-                    <Dropdown.Item>Save...</Dropdown.Item>
-                    <Dropdown.Item>Edit Permissions</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Export</Dropdown.Header>
-                    <Dropdown.Item>Share</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-
-                <Menu.Menu position='right'>
-                  <div className='ui right aligned category search item'>
-                    <div className='ui transparent icon input'>
-                      <input
-                        className='prompt'
-                        type='text'
-                        placeholder='Search animals...'
-                      />
-                      <i className='search link icon' />
-                    </div>
-                    <div className='results' />
-                  </div>
-                </Menu.Menu>
-              </Menu>
-
-              <Segment attached='bottom'>
-                <img src='/images/wireframe/paragraph.png' />
-              </Segment>
-            </div>
-)
 
           <Wrapper className="column">
-              <Code
-                value={source}
-                onChange={(val) => setSource(val)}
-                docName="source.js"
-              />
-              <FileSize>
-                {size}b, {gzip}b
+            <Code
+              value={source}
+              onChange={(val) => setSource(val)}
+              docName="source.js"
+            />
+            <FileSize>
+              {size}b, {gzip}b
           </FileSize>
-              {/* <AST source={source}></AST> */}
-            </Wrapper>
+            {/* <AST source={source}></AST> */}
+          </Wrapper>
         </Grid.Row>
-          {enableCustomPlugin && (
-            <Grid.Row>
-              <Wrapper className="column">
-                <Code
-                  value={customPlugin}
-                  onChange={(val) => setCustomPlugin(val)}
-                  docName="plugin.js"
-                />
-                <Toggle onClick={() => toggleCustomPlugin(false)} />
-              </Wrapper>
-            </Grid.Row>
-          )}
-          {results}
+        {enableCustomPlugin && (
+          <Grid.Row>
+            <Wrapper className="column">
+              <Code
+                value={customPlugin}
+                onChange={(val) => setCustomPlugin(val)}
+                docName="plugin.js"
+              />
+              <Toggle onClick={() => toggleCustomPlugin(false)} />
+            </Wrapper>
+          </Grid.Row>
+        )}
+        {results}
       </Grid>
 
     </Root >
@@ -246,11 +198,11 @@ function useDebounce(value, delay) {
 
   React.useEffect(() => {
     const handler = setTimeout(() => {
-        setDebouncedValue(value);
+      setDebouncedValue(value);
     }, delay);
 
     return () => {
-        clearTimeout(handler);
+      clearTimeout(handler);
     };
   }, [delay, value]);
 
